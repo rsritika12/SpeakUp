8f436bc0f7c150888ff8a183f4af3617ceb9f495
David Bang <davidbang2012@gmail.com>
2016-09-11 00:49:56 -0400

    Fixed Ascii character error

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,url_for,redirect,render_template, flash, session
 import json, urllib2
 
+app=Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret key"
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
@@ -8,7 +9,7 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 @app.route("/", methods = ["POST", "GET"])
 def index():
     return render_template ("index.html")
-    
-app=Flask(__name__)
+
 if __name__ == '__main__':
-    app.run(host = ‘0.0.0.0’, port=8023)
+    app.debug = False
+    app.run(host = '0.0.0.0', port=8023)
